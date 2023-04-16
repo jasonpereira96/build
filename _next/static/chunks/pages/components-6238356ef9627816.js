dacc96f487298ad43bd4fb14b372e562f25c4eb0
jasonpereira96 <jasonpereira074@gmail.com>
2023-04-16 06:34:12 -0500

    adding build

---
+++ _next/static/chunks/pages/components-6238356ef9627816.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[505],{3251:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/components",function(){return u(3167)}])}},function(n){n.O(0,[774,888,179],function(){return n(n.s=3251)}),_N_E=n.O()}]);
